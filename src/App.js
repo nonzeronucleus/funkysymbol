85de01b371bd788e36333cf82323e80c16825125
Ian <nonzeronucleus@ianplumb.org>
2022-03-19 23:43:12 +0000

    Adds basic styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-// import styled from 'styled-components'
+import styled from 'styled-components'
+
+
+const Page = styled.div`
+  // padding: 20px;
+  // background-color:blue;
+  // height: 100%;
+`
+
+const Img = styled.img`
+  padding-top: 40px;
+  padding-bottom:20px;
+`
+
+const Header = styled.header`
+width:100%;
+background-color:cadetblue;
+display: flex;
+
+
+justify-content: center;
+`
+
+const Body = styled.body`
+  padding:20px;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="./funkylogo.png" alt='logo'></img>
-      </header>
-    </div>
+    <Page className="App">
+      <Header className="App-header">
+        <Img src="./funkylogo.png" alt='logo'></Img>
+      </Header>
+      <Body>
+        We make games
+      </Body>
+    </Page>
   );
 }
 
