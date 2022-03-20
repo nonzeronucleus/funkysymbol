6e3557d93de9c0e99f6d27586fcc03e515367d91
Ian <nonzeronucleus@ianplumb.org>
2022-03-20 14:27:36 +0000

    Adds screenshots

--- src/App.js
+++ src/App.js
@@ -2,18 +2,30 @@ import styled from 'styled-components'
 
 
 const Page = styled.div`
-  // padding: 20px;
+@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;400&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Inspiration&display=swap');
+// padding: 20px;
   // background-color:blue;
   // height: 100%;
+
+  font-family: "Inspiration")//, sans-serif;
 `
 
-const Img = styled.img`
+const TitleImg = styled.img`
   padding-top: 40px;
   padding-bottom:20px;
   width: 100%;
   height: 100%;
   object-fit: scale-down;  
 `
+
+const GameImg = styled.img`
+  padding: 10px;
+  width: 20%;
+  // height: 30%;
+  object-fit: scale-down;  
+`
+
 
 const Header = styled.header`
 width:100%;
@@ -26,13 +38,15 @@ justify-content: center;
 
 const Body = styled.body`
   padding:20px;
+  font-size: large;
+  font-family: 'Josefin Sans', sans-serif;
 `
 
 function App() {
   return (
     <Page className="App">
       <Header className="App-header">
-        <Img src="./funkylogo.png" alt='logo'></Img>
+        <TitleImg src="./funkylogo.png" alt='logo'></TitleImg>
       </Header>
       <Body>
         <h1>
@@ -46,6 +60,27 @@ function App() {
         </p>
         <p>
           We are a family who like to make games. Not necessarily good ones. But games nonetheless
+        </p>
+        <h1>
+          Games
+        </h1>
+        <h2>
+          Wordo
+        </h2>
+        <span>
+        <GameImg src="./standard-ss.jpeg" alt="standard"></GameImg>  
+        <GameImg src="./master-ss.jpeg" alt="standard"></GameImg>  
+        <GameImg src="./options-ss.jpeg" alt="standard"></GameImg>  
+        <GameImg src="./stats-ss.jpeg" alt="standard"></GameImg>  
+        </span>
+        <p>
+          Classis word game. Try to figure out the hidden word. Guess a word and see how many correct letters you have, either in the right or the wrong place
+        </p>
+        <p>
+          Multiple modes available - either showing which letters are correct or, for a harder challenge, only how many you've got right
+        </p>
+        <p>
+          Custom modes also available to create your own game
         </p>
       </Body>
     </Page>
